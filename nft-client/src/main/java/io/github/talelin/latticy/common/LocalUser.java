a824cdae3aff21037c320bd928831e8a3f1beda9
yukok <44150264@qq.com>
2023-02-08 14:37:44 +0800

    update

--- nft-client/src/main/java/io/github/talelin/latticy/common/LocalUser.java
+++ nft-client/src/main/java/io/github/talelin/latticy/common/LocalUser.java
@@ -1,9 +1,6 @@
 package io.github.talelin.latticy.common;
 
 
-
-import io.github.talelin.latticy.model.User.ClientUserDO;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,35 +13,36 @@ public class LocalUser {
 
     private static ThreadLocal<Map<String, Object>> local = new ThreadLocal<>();
 
+//todo:设置你的用户表并将实体类代入
 
-    /**
-     * 设置登录用户
-     *
-     * @param user
-     */
-    public static void setLocalUser(ClientUserDO user) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("user", user);
-        LocalUser.local.set(map);
-    }
-
-
-
-    public static ClientUserDO getCmsUser() {
-        Map<String, Object> map = LocalUser.local.get();
-        return (ClientUserDO) map.get("user");
-    }
-
-
-
-    public static <T> T getLocalUser(Class<T> clazz) {
-        return (T) local.get();
-    }
-
-    /**
-     * 清理当前用户
-     */
-    public static void clearLocalUser() {
-        LocalUser.local.remove();
-    }
+//    /**
+//     * 设置登录用户
+//     *
+//     * @param user
+//     */
+//    public static void setLocalUser(ClientUserDO user) {
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("user", user);
+//        LocalUser.local.set(map);
+//    }
+//
+//
+//
+//    public static ClientUserDO getCmsUser() {
+//        Map<String, Object> map = LocalUser.local.get();
+//        return (ClientUserDO) map.get("user");
+//    }
+//
+//
+//
+//    public static <T> T getLocalUser(Class<T> clazz) {
+//        return (T) local.get();
+//    }
+//
+//    /**
+//     * 清理当前用户
+//     */
+//    public static void clearLocalUser() {
+//        LocalUser.local.remove();
+//    }
 }
